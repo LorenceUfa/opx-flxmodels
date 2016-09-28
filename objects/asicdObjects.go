ad4cc50fa9b0eb97d311b76aaebcd55cc9a5293c
ukoranne <ukoranne@snaproute.com>
2016-09-28 11:42:18 -0700

    acl rule object - details for fields

--- objects/asicdObjects.go
+++ objects/asicdObjects.go
@@ -294,7 +294,7 @@ type BufferGlobalStatState struct {
 type Acl struct {
 	baseObj
 	AclName      string   `SNAPROUTE: "KEY", ACCESS:"w",MULTIPLICITY: "*", DESCRIPTION: "Acl name to be used to refer to this ACL"`
-	AclType      string   `DESCRIPTION: "Type can be IP/MAC/SVI"`
+	AclType      string   `DESCRIPTION: "Type can be IP/MAC"`
 	IntfList     []string `DESCRIPTION: "list of IntfRef can be port/lag object"`
 	RuleNameList []string `DESCRIPTION: "List of rules to be applied to this ACL. This should match with AclRule RuleName"`
 	Direction    string   `SNAPROUTE: "IN/OUT direction in which ACL to be applied"`
@@ -309,10 +309,10 @@ type AclRule struct {
 	DestIp      string `DESCRIPTION: "Destination IP address"`
 	SourceMask  string `DESCRIPTION: "Network mask for source IP"`
 	DestMask    string `DESCRIPTION: "Network mark for dest IP"`
-	Action      string `DESCRIPTION: "Type of action (Allow/Deny)", DEFAULT:"Allow", STRLEN:"16"`
-	Proto       string `DESCRIPTION: "Protocol type"`
-	SrcPort     string `DESCRIPTION: "Source Port", DEAULT:0`
-	DstPort     string `DESCRIPTION: "Dest Port", DEFAULT:0`
+	Action      string `DESCRIPTION: "Type of action (ALLOW/DENY)", DEFAULT:"Allow", STRLEN:"16"`
+	Proto       string `DESCRIPTION: "Protocol type TCP/UDP/ICMPv4/ICMPv6"`
+	SrcPort     string `DESCRIPTION: "Source Port(used for mlag)", DEAULT:0`
+	DstPort     string `DESCRIPTION: "Dest Port(used for mlag)", DEFAULT:0`
 	L4SrcPort   int32  `DESCRIPTION: "TCP/UDP source port", DEFAULT:0`
 	L4DstPort   int32  `DESCRIPTION: "TCP/UDP destionation port", DEAULT:0`
 	L4PortMatch string `DESCRIPTION: "match condition can be EQ(equal) , NEQ(not equal), LT(larger than), GT(greater than), RANGE(port range)", DEFAULT:"NA"`
