785252742e90d9e276f9802b27d17a5385c8b0f4
ccordes-snaproute <ccordes@snaproute.com>
2016-10-24 10:27:57 -0700

    added untagged list to vxlan object.  Also made vni a key for the VTEP

--- objects/vxlandObjects.go
+++ objects/vxlandObjects.go
@@ -3,7 +3,7 @@ package objects
 type VxlanInstance struct {
 	baseObj
 	Vni            uint32   `SNAPROUTE: "KEY",  ACCESS:"w", MULTIPLICITY:"*", DESCRIPTION: VXLAN Network Id, MIN: "1" ,  MAX: "16777215"`
-	UntaggedVlanId uint16   `DESCRIPTION: Vlan associated with the untagged traffic.  Used in conjunction with a given VTEP inner-vlan-handling-mode, MIN: "1" ,  MAX: "4094"`
+	UntaggedVlanId []uint16 `DESCRIPTION: Vlan associated with the untagged traffic.  Used in conjunction with a given VTEP inner-vlan-handling-mode, MIN: "1" ,  MAX: "4094"`
 	VlanId         []uint16 `DESCRIPTION: Vlan associated with the Access targets.  Used in conjunction with a given VTEP inner-vlan-handling-mode, MIN: "1" ,  MAX: "4094"`
 	AdminState     string   `DESCRIPTION: Administrative state of VXLAN layer, UP will allow for traffic to be processed in the VNI, DOWN will drop traffic within this layer, STRLEN:"4", SELECTION: UP/DOWN, DEFAULT: UP`
 }
@@ -18,12 +18,12 @@ type VxlanInstanceState struct {
 type VxlanVtepInstance struct {
 	baseObj
 	Intf                  string `SNAPROUTE: "KEY", ACCESS:"w", MULTIPLICITY:"*", DESCRIPTION: VTEP instance identifier name. should be defined as either vtep<id#> or <id#> if the later then 'vtep' will be prepended to the <id#> example: vtep100 or 100`
+	Vni                   uint32 `SNAPROUTE: "KEY", DESCRIPTION: VXLAN Network ID, MIN: "1" ,  MAX: "16777215"`
 	IntfRef               string `DESCRIPTION: Source interface where the source ip will be derived from.  If an interface is not supplied the src-ip will be used. This attribute takes presedence over src-ip attribute.`
 	DstUDP                uint16 `DESCRIPTION: vxlan udp port.  Deafult is the iana default udp port, DEFAULT: 4789`
 	TTL                   uint16 `DESCRIPTION: TTL of the Vxlan tunnel, MIN:0, MAX:255, DEFAULT: 64`
 	TOS                   uint16 `DESCRIPTION: Type of Service, MIN:0, MAX:255, DEFAULT: 0`
 	InnerVlanHandlingMode int32  `DESCRIPTION: The inner vlan tag handling mode., SELECTION: DISCARD_INNER_VLAN(0)/NO_DISCARD_INNER_VLAN(1), DEFAULT: 0`
-	Vni                   uint32 `DESCRIPTION: VXLAN Network ID, MIN: "1" ,  MAX: "16777215"`
 	DstIp                 string `DESCRIPTION: Destination IP address for the static VxLAN tunnel"`
 	SrcIp                 string `DESCRIPTION: Source IP address for the VxLAN tunnel, if this is supplied it is assumed that the intf-ref is this vtep.  This  attribute will be ignored if intf-ref is set", DEFAULT: "0.0.0.0"`
 	VlanId                uint16 `DESCRIPTION: Vlan Id to encapsulate with the vtep tunnel ethernet header`
